ff670aa8a62c99ff9cfa8e81fd9a4f67b13f853a
pdelcogliano <pdelco@hotmail.com>
2021-02-28 16:43:04 -0500

    refactoring SlugsResourceParameters

---
+++ m-url.data/ResourceParameters/PagingResourceParameters.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace M_url.Data.ResourceParameters
+{
+    public class PagingResourceParameters
+    {
+        const int maxPageSize = 20;
+
+        protected int m_pageSize = 10;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize 
+        {
+            get => m_pageSize;
+            set => m_pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+}
