2380e576ec44d4ec123b550a6620708f99955f48
Paul Delcogliano <pdelco@gmail.com>
2021-03-01 10:51:44 -0500

    added x-frame-options and strict-transport-security headers

--- m-url.api/Startup.cs
+++ m-url.api/Startup.cs
@@ -26,7 +26,6 @@ namespace M_url.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers(setupAction => 
             {
                 setupAction.ReturnHttpNotAcceptable = true;     // sends 406 for unsupported output formats, i.e. zip or other formats
@@ -42,6 +41,8 @@ namespace M_url.Api
             services.AddDbContext<MurlContext>(options =>
               options.UseSqlServer(connectionString, x => x.MigrationsAssembly("M-url.Data.Migrations")));
             services.AddScoped<IMurlRepository, MurlRepository>();
+
+            services.AddAntiforgery(o => o.SuppressXFrameOptionsHeader = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +54,7 @@ namespace M_url.Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "M_url.Api v1"));
             }
-
+            app.UseHsts();
             app.UseHttpsRedirection();
 
             app.UseRouting();
