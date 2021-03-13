5b0bc3858128a7c2604ec6d3d5b853a59a2251ca
Paul Delcogliano <pdelco@gmail.com>
2021-03-13 09:27:28 -0500

    ability to add bulk urls with system generated slugs

--- m-url.api/Controllers/SlugCollectionsController.cs
+++ m-url.api/Controllers/SlugCollectionsController.cs
@@ -9,6 +9,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using M_url.Services;
 
 namespace M_url.Api.Controllers
 {
@@ -18,11 +21,18 @@ namespace M_url.Api.Controllers
     [Consumes(MediaTypeNames.Application.Json)]
     public class SlugCollectionsController : ControllerBase
     {
+        private readonly ILogger<SlugCollectionsController> _logger;
+        private readonly IConfiguration _configuration;
         private readonly IMurlRepository _murlRepository;
 
-        public SlugCollectionsController(IMurlRepository murlRepository)
+        public SlugCollectionsController(ILogger<SlugCollectionsController> logger, IConfiguration configuration, IMurlRepository murlRepository)
         {
-            _murlRepository = murlRepository ?? throw new ArgumentNullException(nameof(murlRepository));
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger)); //.CreateLogger();
+            _configuration = configuration ??
+                    throw new ArgumentNullException(nameof(configuration));
+            _murlRepository = murlRepository ?? 
+                throw new ArgumentNullException(nameof(murlRepository));
         }
 
         //  api.slugcollections/(slug1, slug2)
@@ -33,15 +43,20 @@ namespace M_url.Api.Controllers
         /// <returns></returns>
         [HttpGet("({slugs})", Name = "GetSlugCollection")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> GetSlugCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<string> slugs)
         {
+            if (slugs == null)
+                return BadRequest();
+
             IEnumerable<SlugEntity> slugCollection = await _murlRepository.GetAllSlugsAsync(slugs);
 
-            if (slugCollection.Count() != slugs.Count())
-                return NotFound();
+            if (slugCollection.Count() == slugs.Count())
+                return Ok(slugCollection);
 
-            return Ok(slugCollection);
+            // return 404 if number of slugs to find doesn't equal number of slugs found.
+            return NotFound();
         }
 
         /// <summary>
@@ -51,15 +66,22 @@ namespace M_url.Api.Controllers
         /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> CreateSlugCollection(IEnumerable<SlugForCreationDto> slugsToAdd)
         {
+            _logger.LogInformation($"bulk add new slugs");
+
+            if (slugsToAdd == null || slugsToAdd.Count() == 0)
+                return BadRequest();
+            
             List<SlugEntity> slugsToReturn = new List<SlugEntity>();
+            int slugLength = _configuration.GetValue<short>("SlugLength", 5);
 
             foreach (var slug in slugsToAdd)
             {
-                SlugEntity newSlug = new SlugEntity()
+                SlugEntity newSlug = new SlugEntity
                 {
-                    Slug = slug.Slug,
+                    Slug = string.IsNullOrWhiteSpace(slug.Slug) ? NanoidService.Create(slugLength) : slug.Slug,
                     Url = slug.Url
                 };
                 _murlRepository.AddSlug(newSlug);
